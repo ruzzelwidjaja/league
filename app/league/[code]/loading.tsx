891a878c88400c01e985b30ae91c62e2141bb0aa
Ruzzel Widjaja <ruzzelwidjaja@gmail.com>
2025-06-05 12:37:23 +0100

    refactor: loading spinner

--- app/league/[code]/loading.tsx
+++ app/league/[code]/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loading() {
   return (
     <div className="min-h-svh flex items-center justify-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+      <div className="h-20 w-20 border-8 border-border-200 text-secondary inline-block animate-spin rounded-full border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status"></div>
     </div>
   );
 }
